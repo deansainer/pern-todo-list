4739abc9964eba66420320e6f1c2513d3b8078ba
deansainer <determind2k@gmail.com>
2023-12-15 15:14:01 +0100

    implemented update and delete functions in controller

--- controllers/TodoController.js
+++ controllers/TodoController.js
@@ -6,15 +6,28 @@ class TodoController {
     const todos = await db.query("select * from todo;");
     res.json(todos.rows);
   }
-
+  
   async get_todo(req, res) {
     const { id } = req.params;
     const todo = await db.query('select * from todo where id = $1', [id])
+    res.json(todo.rows[0])
   }
 
   async create_todo(req, res) {
     const {description} = req.body;
     const todo = await db.query('insert into todo (description) values ($1)', [description])
+    res.json(todo.rows[0])
+  }
+
+  async delete_todo(req, res){
+    const {id} = req.params;
+    const todo = await db.query('delete from todo where id=$1', [id])
+    res.json(todo.rows[0])
+  }
+
+  async update_todo(req, res) {
+    const {description, id} = req.body;
+    const todo = await db.query('update todo set description=$1 where id=$2', [description, id])
     res.json(todo.rows[0])
   }
 
--- routes/TodoRouter.js
+++ routes/TodoRouter.js
@@ -5,5 +5,7 @@ const TodoController = require("../controllers/TodoController");
 router.get("/todos", TodoController.get_todos);
 router.get("/todos/:id", TodoController.get_todo);
 router.post("/todos", TodoController.create_todo);
+router.delete("/todos/:id", TodoController.delete_todo);
+router.put("/todos", TodoController.update_todo);
 
 module.exports = router;
